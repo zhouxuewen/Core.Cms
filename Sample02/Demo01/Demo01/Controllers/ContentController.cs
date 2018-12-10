6e7a25a3b012a0faf58050f120cd967da40025b4
zhouxuewen <464194769@qq.com>
2018-12-10 19:41:25 +0800

    Core

---
+++ Sample02/Demo01/Demo01/Controllers/ContentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo01.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Demo01.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+        
+    }
+}
---
+++ Sample02/Demo01/Demo01/Models/ContentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo01.Models
+{
+    public class ContentViewModel
+    {
+        
+        
+            /// <summary>
+            /// 内容列表
+            /// </summary>
+            public List<Content> Contents { get; set; }
+      
+    }
+}
